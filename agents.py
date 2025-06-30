55e91deaf656e9dd77508fcb6a28ce8053bf8e20
SouhailFinTech <hrich.souhail5@gmail.com>
2025-06-30 13:57:16 +0100

    Update agents.py

--- agents.py
+++ agents.py
@@ -1,25 +1,18 @@
-# agents.py
-
 from config import llm_config
-
 import autogen
 
 financial_assistant = autogen.AssistantAgent(
     name="Financial_assistant",
     llm_config=llm_config,
-    system_message="You are a financial analyst..."
 )
 
 research_assistant = autogen.AssistantAgent(
     name="Researcher",
     llm_config=llm_config,
-    system_message="You are a researcher..."
 )
 
 writer = autogen.AssistantAgent(
     name="Writer",
     llm_config=llm_config,
-    system_message="You are a professional writer..."
+    system_message="""You are a professional writer...""",
 )
-
-# Define critic, reviewers, etc.
